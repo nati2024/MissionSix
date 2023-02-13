316c98c329b4c0b3d3f52506a824185f35d1a721
nati2024 <95377889+nati2024@users.noreply.github.com>
2023-02-13 15:43:13 -0700

    Add project files.

---
+++ MissionSix/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MissionSix.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionSix.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieApplicationContext blahContext { get; set; }
+
+        //Constructor
+        public HomeController(ILogger<HomeController> logger, MovieApplicationContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+
+        public IActionResult Extra()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View("MovieForm");
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(ApplicationResponse ar)
+        {
+            if (ModelState.IsValid)
+            {
+
+                blahContext.Add(ar);
+                blahContext.SaveChanges();
+
+                return View("Confirmation", ar);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MissionSix/Models/MovieApplicationContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionSix.Models
+{
+    public class MovieApplicationContext : DbContext
+    {
+        //Constructor
+        public MovieApplicationContext(DbContextOptions<MovieApplicationContext> options) : base (options)
+        {
+            //leave blank for now
+        }
+        public DbSet<ApplicationResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationResponse>().HasData(
+
+                new ApplicationResponse
+                {
+                    ApplicationId = 1,
+                    Category = "Drama",
+                    Title = "Pride and Prejudice",
+                    Year = 2005,
+                    Director = "Joe Wright",
+                    Edited = true,
+                    Rating = "PG-13"
+                },
+                new ApplicationResponse
+                {
+                    ApplicationId = 2,
+                    Category = "television",
+                    Title = "New Girl",
+                    Year = 2011,
+                    Director = "Fox",
+                    Edited = true,
+                    Rating = "PG-13"
+                },
+                new ApplicationResponse
+                {
+                    ApplicationId = 3,
+                    Category = "Family",
+                    Title = "Toy Story",
+                    Year = 1995,
+                    Director = "John Lasseter",
+                    Edited = true,
+                    Rating = "PG"
+                }
+                );
+        }
+    }
+}
